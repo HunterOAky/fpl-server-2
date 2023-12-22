6fcfe22e03c8bf742f914044a53f11e61b922772
Aaron Okoroh <aaron.okoroh@eckoh.com>
2023-12-22 12:37:14 +0000

    add new stuff

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const axios = require('axios');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+const fplApiUrl = 'https://fantasy.premierleague.com/api/bootstrap-static/';
+
+app.get('/', async (req, res) => {
+  try {
+    // Make a GET request to the FPL API
+    const response = await axios.get(fplApiUrl);
+
+    // Extract relevant data from the API response
+    const responseData = response.data;
+
+    // Send the data as the response
+    res.json(responseData);
+  } catch (error) {
+    // Handle errors
+    console.error('Error fetching data from FPL API:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
