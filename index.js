4b93b0d706e8fb667d80ba4f647c22db1e6456c4
Aaron Okoroh <aaron.okoroh@eckoh.com>
2023-12-22 14:56:18 +0000

    new shit

--- index.js
+++ index.js
@@ -3,31 +3,86 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
-const apiUrl = 'https://fplgameweekupdateleague.azurewebsites.net/api/LeagueFunction';
+const apiUrl = 'https://apin.livefpltables.com/';
 
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
   try {
-    // Make a GET request to the specified API endpoint with the given headers
-    const response = await axios.get(apiUrl, {
-      params: {
-        leagueId: 279926,
-        entry: 2675959,
-        compareEntry: 2675959,
-        currentweek: 2,
-        doNotCache: 0,
-        refreshEntry: 0,
-        filterFrom: 0,
-        filterTo: 0,
-        liveFeedEventTimestamp: '',
-        fetchMoreTeams: undefined,
-        prevSeasonNum: 5,
-        sortOrder: 'orderByNet',
-        currentPage: 1,
-      },
-      headers: {
-        FunctionKey: 'db059d47-8b44-476a-9dfc-509bceb87bee',
-      },
-    });
+    // Define the GraphQL query in the request body
+    const requestBody = {
+      query: `
+        {
+          entry(id: 2675959) {
+            id
+            name
+            player_first_name
+            player_last_name
+            last_deadline_total_transfers
+            started_event
+          }
+
+          entryHistory(id: 2675959) {
+            current {
+              total_points
+              event_transfers_cost
+              overall_rank
+              event_transfers
+              points
+              rank
+              bank
+              value
+            }
+            chips {
+              name
+              event
+            }
+            past {
+              season_name
+              total_points
+              rank
+            }
+          }
+
+          picks(entry: 2675959, event: 18) {
+            active_chip
+            entry_history {
+              event_transfers
+              event_transfers_cost
+            }
+            picks {
+              player {
+                id
+                web_name
+              }
+              position
+              multiplier
+              original_multiplier
+              is_captain
+              is_vice_captain
+            }
+          }
+
+          entryTransfers(entry: 2675959, event: 18) {
+            transfer {
+              element_in {
+                web_name
+                id
+              }
+              element_out {
+                web_name
+                id
+              }
+            }
+          }
+
+          entryNetTransferPoints(entry: 2675959, event: 18) {
+            netTransferPoints
+          }
+        }
+      `,
+    };
+
+    // Make a POST request to the specified API endpoint
+    const response = await axios.post(apiUrl, requestBody);
 
     // Extract relevant data from the API response
     const responseData = response.data;
@@ -36,11 +91,13 @@ app.get('/', async (req, res) => {
     res.json(responseData);
   } catch (error) {
     // Handle errors
-    console.error('Error making GET request:', error.message);
-    res.status(500).send('Internal Server Error');
+    console.error('Error making POST request:', error.message);
+
+    // Send an error response with a 500 status code
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Express server listening on port ${port}`);
 });
