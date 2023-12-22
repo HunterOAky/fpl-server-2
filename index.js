8922da871159f072971697b1f8c994f01842037b
Aaron Okoroh <aaron.okoroh@eckoh.com>
2023-12-22 13:47:37 +0000

    try non official api

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
-const fplApiUrl = 'https://fantasy.premierleague.com/api/bootstrap-static/';
-
-// Add the provided cookie to the request headers
-const axiosWithCookies = axios.create({
-  headers: {
-    'Cookie': 'datadome=trbmkvlFw5w6SkAOL4ivZHvjTdtbcGmh_OdaUYhPZ~7_1m6YoSKFoBAI8LLkKO2g981uDGSCPjby_7i2SODdLeWaYBa4FHkU~kd1K9Y_LVwoonofXGqfCIVcMnbGxm3e; pl_profile="eyJzIjogIld6SXNOelU1TWprMk56UmQ6MXJHZkR1OmZSR3QydW5CSzJzbE1OTWszNzdNWkVXcFI4TlRtckxGMTNkSkM1Y1FaUDQiLCAidSI6IHsiaWQiOiA3NTkyOTY3NCwgImZuIjogIkFhcm9uIiwgImxuIjogIk9rb3JvaCIsICJmYyI6IDN9fQ=="; csrftoken=lpj5wOUKbC7JspidoTMu7Nxc5xngDe22; sessionid=.eJxVy8sKwjAQheF3yVrKpJnc3LkXFIrrkMkkRCylGLsS3910p8vDd_63CHF71bC1_Ax3FkdhtR-9sSgOv0QxPfKy-zqXdR52Ga7nW7c2TZdTn_9Bja32t7QAlBwrsgjRIFEsDCyRWWbjJYNxAKw9oTI0RquT0ly4WCzeOSk-XwpxMzQ:1rGfDu:5929-nIma9IbKmDAkD-EKaMglAaif67gWMPSwth3hyY',
-  },
-});
+const apiUrl = 'https://fplgameweekupdateleague.azurewebsites.net/api/LeagueFunction';
 
 app.get('/', async (req, res) => {
   try {
-    // Make a GET request to the FPL API using the Axios instance
-    const response = await axiosWithCookies.get(fplApiUrl);
+    // Make a GET request to the specified API endpoint with the given headers
+    const response = await axios.get(apiUrl, {
+      params: {
+        leagueId: 279926,
+        entry: 2675959,
+        compareEntry: 2675959,
+        currentweek: 2,
+        doNotCache: 0,
+        refreshEntry: 0,
+        filterFrom: 0,
+        filterTo: 0,
+        liveFeedEventTimestamp: '',
+        fetchMoreTeams: undefined,
+        prevSeasonNum: 5,
+        sortOrder: 'orderByNet',
+        currentPage: 1,
+      },
+      headers: {
+        FunctionKey: 'db059d47-8b44-476a-9dfc-509bceb87bee',
+      },
+    });
 
     // Extract relevant data from the API response
     const responseData = response.data;
@@ -24,7 +36,7 @@ app.get('/', async (req, res) => {
     res.json(responseData);
   } catch (error) {
     // Handle errors
-    console.error('Error fetching data from FPL API:', error.message);
+    console.error('Error making GET request:', error.message);
     res.status(500).send('Internal Server Error');
   }
 });
